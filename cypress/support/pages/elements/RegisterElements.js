86e93a4ace0a33bcdcfbf1ad7e25d7a7c688a381
João Melo <vivitomelo@gmail.com>
2025-12-03 17:06:21 -0300

    test(gui): implement user registration spec

--- cypress/support/pages/elements/RegisterElements.js
+++ cypress/support/pages/elements/RegisterElements.js
@@ -1,10 +1,10 @@
 class RegisterElements {
     nameInput = () => cy.get('[data-testid="nome"]');
     emailInput = () => cy.get('[data-testid="email"]');
-    passwordInput = () => cy.get('[data-testid="senha"]');
+    passwordInput = () => cy.get('[data-testid="password"]');
     adminCheckbox = () => cy.get('[data-testid="checkbox"]');
-    submitButton = () => cy.get('[data-testid="entrar"]');
-    alertMessage = () => cy.get('.close.btn-close-succcess-alert');
+    submitButton = () => cy.get('[data-testid="cadastrar"]');
+    alertMessage = () => cy.contains('a.alert-link', 'Cadastro realizado com sucesso');
 }
 
 export default new RegisterElements();
