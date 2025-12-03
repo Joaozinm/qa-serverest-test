383e76b4f7cf5974f013d91afa44cb550c99cdde
João Melo <vivitomelo@gmail.com>
2025-12-03 16:19:12 -0300

    feat(api): create API helper commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { faker } from '@faker-js/faker';
+
+Cypress.Commands.add('api_createUser', () => {
+    const user = {
+        nome: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: "1234",
+        administrador: "true"
+    };
+
+    return cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/usuarios',
+        body: user,
+        failOnStatusCode: false
+    }).then(res => {
+        return { res, user };
+    });
+});
+
+Cypress.Commands.add('api_login', (email, password) => {
+    return cy.request({
+        method: 'POST',
+        url: 'https://serverest.dev/login',
+        body: { email, password },
+        failOnStatusCode: false
+    });
+});
